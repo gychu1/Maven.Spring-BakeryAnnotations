f5b975f254ad97d88624258a9464458483fafc5a
Git-Yee Chu <chugityee2@yahoo.com>
2022-08-12 00:29:59 -0400

    fixed path name, all tests passed

--- src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("baker")
+@RequestMapping("bakers")
 public class BakerController {
     private BakerService service;
 

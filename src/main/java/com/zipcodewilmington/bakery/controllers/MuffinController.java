48727d4c5c18c7bab03d7500ae47b0471d7ed45c
Git-Yee <git-yee@ZipcodersMBP58.fios-router.home>
2022-08-11 23:32:33 -0400

    annotated bakercontroller

--- src/main/java/com/zipcodewilmington/bakery/controllers/MuffinController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/MuffinController.java
@@ -4,7 +4,11 @@ import com.zipcodewilmington.bakery.models.Muffin;
 import com.zipcodewilmington.bakery.services.MuffinService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("muffin")
 public class MuffinController {
     private MuffinService service;
 
